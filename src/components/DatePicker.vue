9b6da40deb9f64e576427106d8ec592f19c6bd65
bglupi <bglupi@ncomz.com>
2021-04-05 11:31:19 +0900

    test

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -8,17 +8,14 @@
         min-width="290px"
     >
         <template v-slot:activator="{ on }">
-        <v-text-field
-            v-model="newdate"
-            label="달력을 선택하세요"
-            readonly
-            solo
-            v-on="on"
-        >
-            <template v-slot:append>
-                <img width="18" height="19" src="@/assets/cal.svg" @click="menu = true" style="cursor:pointer"/>
-            </template>
-        </v-text-field>
+            <v-text-field
+                v-model="newdate"
+                label="YYYY-MM-DD"
+                readonly
+                solo
+                v-on="on"
+            >
+            </v-text-field>
         </template>
         <v-date-picker v-model="newdate" @input="input" header-color="primary"></v-date-picker>
     </v-menu>
@@ -59,6 +56,16 @@ export default {
 </script>
 
 <style>
+.v-input{width:160px; display:inline-block; vertical-align: middle;}
+.v-input__slot{height:auto; padding:0 !important; box-shadow:none !important; margin:0 !important;}
+.v-input input{margin:0 !important; max-height:inherit; height:50px; line-height:48px; border:1px solid #dddddd; border-radius:2px; padding:0 50px 0 20px; background:#ffffff url(../assets/images/ico_datePicker.png) no-repeat center right 10px;}
+.v-text-field.v-text-field--solo .v-input__control{min-height:auto;}
+.v-text-field.v-text-field--enclosed .v-text-field__details{width:0; height:0; padding:0; margin:0; min-height:0; display:block;}
+.icoDate{width:22px; height:22px; background:url(../assets/images/ico_datePicker.png) no-repeat 0 0;}
+.v-input .v-label{font-size:16px; color:#a0b0b6; padding:0 50px 0 20px; max-width:inherit;}
+.v-menu__content{transform:translateX(-40px);}
+.v-input input:focus{color:#0c323d; border-color:#2cbbb7; background-image:url(../assets/images/ico_datePickerHover.png);}
+
 .v-picker--date .primary {
     background-color: #1867c0 !important;
     border-color: #1867c0 !important;
